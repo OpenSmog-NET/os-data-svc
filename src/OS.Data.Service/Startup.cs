6d4fd2c0314d0a208185e372b6d769183afac9d4
Maciej Misztal <mmisztal1980@gmail.com>
2017-12-02 14:40:42 +0100

    connection strings removed

--- src/OS.Data.Service/Startup.cs
+++ src/OS.Data.Service/Startup.cs
@@ -7,26 +7,6 @@ namespace OS.Data.Service
 {
     public class Startup
     {
-        //public static IConfigurationRoot ReadConfiguration()
-        //{
-        //    var builder = new ConfigurationBuilder()
-        //        .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-        //        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-        //        //.AddJsonFile($"appsettings.{WebJobEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
-        //        //.AddWhen(() => !WebJobEnvironment.IsDevelopment, (cfg) => cfg.AddKeyVault())
-        //        .AddEnvironmentVariables();
-
-        //    return builder.Build();
-        //}
-
-        //public static IServiceProvider ConfigureServices(IConfigurationRoot configuration)
-        //{
-        //    var services = new ServiceCollection()
-        //        .AddSingleton(configuration)
-        //        .AddSingleton<DataService>();
-
-        //    return services.BuildServiceProvider();
-        //}
         public void ConfigureServices(IConfiguration configuration, IServiceCollection services, ILoggerFactory loggerFactory)
         {
             services.AddLogging();
